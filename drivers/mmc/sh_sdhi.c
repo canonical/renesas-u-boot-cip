canonical/renesas-u-boot-cip

commit 46b7dae09153a0c0d028dd1f5d09bda11fa907c1

RZT2H and RZN2H sdhi updates to use mach-renesas
sd card u-boot driver updates

Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

drivers/mmc/sh_sdhi.c
@@ -22,8 +22,8 @@
 #include <linux/compat.h>
 #include <linux/io.h>
 #include <linux/sizes.h>
-#ifdef CONFIG_ARCH_RMOBILE
-#include <asm/arch/rmobile.h>
+#ifdef CONFIG_ARCH_RENESAS
+#include <asm/arch/renesas.h>
 #endif
 #ifdef CONFIG_RZF_DEV
 #include <asm/arch-rzmpu/sh_sdhi.h>
@@ -863,11 +863,12 @@ static int sh_sdhi_dm_probe(struct udevice *dev)
 	struct sh_sdhi_plat *plat = dev_get_plat(dev);
 	struct sh_sdhi_host *host = dev_get_priv(dev);
 	struct mmc_uclass_priv *upriv = dev_get_uclass_priv(dev);
-	struct clk sh_sdhi_clk;
 	const u32 quirks = dev_get_driver_data(dev);
 	fdt_addr_t base;
+#if !((defined CONFIG_R9A09G077) || (defined CONFIG_R9A09G087))
+	struct clk sh_sdhi_clk;
 	int ret;
-
+#endif
 	base = dev_read_addr(dev);
 	if (base == FDT_ADDR_T_NONE)
 		return -EINVAL;
@@ -921,7 +922,7 @@ static int sh_sdhi_dm_probe(struct udevice *dev)
 
 #if ((defined CONFIG_R9A09G077) || (defined CONFIG_R9A09G087))
 	sh_sdhi_writel(host, SDHI_SD_STATUS, ~SD_STATUS_SD_PWEN & sh_sdhi_readl(host, SDHI_SD_STATUS));
-	mdelay(6);
+	mdelay(5);
 	sh_sdhi_writel(host, SDHI_SD_STATUS, SD_STATUS_SD_PWEN);
 #endif
 	sh_sdhi_initialize_common(host);

arch/arm/mach-rmobile/include/mach/sh_sdhi.h
@@ -1,181 +1,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * drivers/mmc/sh-sdhi.h
- *
- * SD/MMC driver for Renesas rmobile ARM SoCs
- *
- * Copyright (C) 2013-2017 Renesas Electronics Corporation
- * Copyright (C) 2008-2009 Renesas Solutions Corp.
- */
-
-#ifndef _SH_SDHI_H
-#define _SH_SDHI_H
-
-#include <linux/bitops.h>
-#define SDHI_CMD			(0x0000 >> 1)
-#define SDHI_PORTSEL			(0x0004 >> 1)
-#define SDHI_ARG0			(0x0008 >> 1)
-#define SDHI_ARG1			(0x000C >> 1)
-#define SDHI_STOP			(0x0010 >> 1)
-#define SDHI_SECCNT			(0x0014 >> 1)
-#define SDHI_RSP00			(0x0018 >> 1)
-#define SDHI_RSP01			(0x001C >> 1)
-#define SDHI_RSP02			(0x0020 >> 1)
-#define SDHI_RSP03			(0x0024 >> 1)
-#define SDHI_RSP04			(0x0028 >> 1)
-#define SDHI_RSP05			(0x002C >> 1)
-#define SDHI_RSP06			(0x0030 >> 1)
-#define SDHI_RSP07			(0x0034 >> 1)
-#define SDHI_INFO1			(0x0038 >> 1)
-#define SDHI_INFO2			(0x003C >> 1)
-#define SDHI_INFO1_MASK			(0x0040 >> 1)
-#define SDHI_INFO2_MASK			(0x0044 >> 1)
-#define SDHI_CLK_CTRL			(0x0048 >> 1)
-#define SDHI_SIZE			(0x004C >> 1)
-#define SDHI_OPTION			(0x0050 >> 1)
-#define SDHI_ERR_STS1			(0x0058 >> 1)
-#define SDHI_ERR_STS2			(0x005C >> 1)
-#define SDHI_BUF0			(0x0060 >> 1)
-#define SDHI_SDIO_MODE			(0x0068 >> 1)
-#define SDHI_SDIO_INFO1			(0x006C >> 1)
-#define SDHI_SDIO_INFO1_MASK		(0x0070 >> 1)
-#define SDHI_CC_EXT_MODE		(0x01B0 >> 1)
-#define SDHI_SOFT_RST			(0x01C0 >> 1)
-#define SDHI_VERSION			(0x01C4 >> 1)
-#define SDHI_HOST_MODE			(0x01C8 >> 1)
-#define SDHI_SDIF_MODE			(0x01CC >> 1)
-#define SDHI_SD_STATUS			(0x01E4 >> 1)	/* Only support RZ/T2H */
-#define SDHI_EXT_SWAP			(0x01E0 >> 1)
-#define SDHI_SD_DMACR			(0x0324 >> 1)
-
-/* SDHI CMD VALUE */
-#define CMD_MASK			0x0000ffff
-
-/* SDHI_PORTSEL */
-#define USE_1PORT			(1 << 8) /* 1 port */
-
-/* SDHI_ARG */
-#define ARG0_MASK			0x0000ffff
-#define ARG1_MASK			0x0000ffff
-
-/* SDHI_STOP */
-#define STOP_SEC_ENABLE			(1 << 8)
-
-/* SDHI_INFO1 */
-#define INFO1_RESP_END			(1 << 0)
-#define INFO1_ACCESS_END		(1 << 2)
-#define INFO1_CARD_RE			(1 << 3)
-#define INFO1_CARD_IN			(1 << 4)
-#define INFO1_ISD0CD			(1 << 5)
-#define INFO1_WRITE_PRO			(1 << 7)
-#define INFO1_DATA3_CARD_RE		(1 << 8)
-#define INFO1_DATA3_CARD_IN		(1 << 9)
-#define INFO1_DATA3			(1 << 10)
-
-/* SDHI_INFO2 */
-#define INFO2_CMD_ERROR			(1 << 0)
-#define INFO2_CRC_ERROR			(1 << 1)
-#define INFO2_END_ERROR			(1 << 2)
-#define INFO2_TIMEOUT			(1 << 3)
-#define INFO2_BUF_ILL_WRITE		(1 << 4)
-#define INFO2_BUF_ILL_READ		(1 << 5)
-#define INFO2_RESP_TIMEOUT		(1 << 6)
-#define INFO2_SDDAT0			(1 << 7)
-#define INFO2_BRE_ENABLE		(1 << 8)
-#define INFO2_BWE_ENABLE		(1 << 9)
-#define INFO2_CBUSY			(1 << 14)
-#define INFO2_ILA			(1 << 15)
-#define INFO2_ALL_ERR			(0x807f)
-
-/* SDHI_INFO1_MASK */
-#define INFO1M_RESP_END			(1 << 0)
-#define INFO1M_ACCESS_END		(1 << 2)
-#define INFO1M_CARD_RE			(1 << 3)
-#define INFO1M_CARD_IN			(1 << 4)
-#define INFO1M_DATA3_CARD_RE		(1 << 8)
-#define INFO1M_DATA3_CARD_IN		(1 << 9)
-#define INFO1M_ALL			(0xffff)
-#define INFO1M_SET			(INFO1M_RESP_END |	\
-					INFO1M_ACCESS_END |	\
-					INFO1M_DATA3_CARD_RE |	\
-					INFO1M_DATA3_CARD_IN)
-
-/* SDHI_INFO2_MASK */
-#define INFO2M_CMD_ERROR		(1 << 0)
-#define INFO2M_CRC_ERROR		(1 << 1)
-#define INFO2M_END_ERROR		(1 << 2)
-#define INFO2M_TIMEOUT			(1 << 3)
-#define INFO2M_BUF_ILL_WRITE		(1 << 4)
-#define INFO2M_BUF_ILL_READ		(1 << 5)
-#define INFO2M_RESP_TIMEOUT		(1 << 6)
-#define INFO2M_BRE_ENABLE		(1 << 8)
-#define INFO2M_BWE_ENABLE		(1 << 9)
-#define INFO2M_ILA			(1 << 15)
-#define INFO2M_ALL			(0xffff)
-#define INFO2M_ALL_ERR			(0x807f)
-
-/* SDHI_CLK_CTRL */
-#define CLK_ENABLE			(1 << 8)
-
-/* SDHI_OPTION */
-#define OPT_BUS_WIDTH_M			(5 << 13)	/* 101b (15-13bit) */
-#define OPT_BUS_WIDTH_1			(4 << 13)	/* bus width = 1 bit */
-#define OPT_BUS_WIDTH_4			(0 << 13)	/* bus width = 4 bit */
-#define OPT_BUS_WIDTH_8			(1 << 13)	/* bus width = 8 bit */
-
-/* SDHI_ERR_STS1 */
-#define ERR_STS1_CRC_ERROR		((1 << 11) | (1 << 10) | (1 << 9) | \
-					(1 << 8) | (1 << 5))
-#define ERR_STS1_CMD_ERROR		((1 << 4) | (1 << 3) | (1 << 2) | \
-					(1 << 1) | (1 << 0))
-
-/* SDHI_ERR_STS2 */
-#define ERR_STS2_RES_TIMEOUT		(1 << 0)
-#define ERR_STS2_RES_STOP_TIMEOUT	((1 << 0) | (1 << 1))
-#define ERR_STS2_SYS_ERROR		((1 << 6) | (1 << 5) | (1 << 4) | \
-					(1 << 3) | (1 << 2) | (1 << 1) | \
-					(1 << 0))
-
-/* SDHI_SDIO_MODE */
-#define SDIO_MODE_ON			(1 << 0)
-#define SDIO_MODE_OFF			(0 << 0)
-
-/* SDHI_SDIO_INFO1 */
-#define SDIO_INFO1_IOIRQ		(1 << 0)
-#define SDIO_INFO1_EXPUB52		(1 << 14)
-#define SDIO_INFO1_EXWT			(1 << 15)
-
-/* SDHI_SDIO_INFO1_MASK */
-#define SDIO_INFO1M_CLEAR		((1 << 1) | (1 << 2))
-#define SDIO_INFO1M_ON			((1 << 15) | (1 << 14) | (1 << 2) | \
-					 (1 << 1) | (1 << 0))
-
-/* SDHI_EXT_SWAP */
-#define SET_SWAP			((1 << 6) | (1 << 7))	/* SWAP */
-
-/* SDHI_SOFT_RST */
-#define SOFT_RST_ON			(0 << 0)
-#define SOFT_RST_OFF			(1 << 0)
-
-/* SDHI_SD_STATUS */
-#define SD_STATUS_SD_PWEN		(1 << 0)
-#define SD_STATUS_SD_RST		(1 << 1)
-#define SD_STATUS_SD_IOVS		(1 << 16)
-
-#define	CLKDEV_SD_DATA			25000000	/* 25 MHz */
-#define CLKDEV_HS_DATA			50000000	/* 50 MHz */
-#define CLKDEV_MMC_DATA			20000000	/* 20MHz */
-#define	CLKDEV_INIT			400000		/* 100 - 400 KHz */
-
-/* For quirk */
-#define SH_SDHI_QUIRK_16BIT_BUF		BIT(0)
-#define SH_SDHI_QUIRK_64BIT_BUF		BIT(1)
-
-/* SDHI channel number defined based on specific SoC */
-#if defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U)
-#define CONFIG_SYS_SH_SDHI_NR_CHANNEL 2
-#endif
-
-int sh_sdhi_init(unsigned long addr, int ch, unsigned long quirks);
-
-#endif /* _SH_SDHI_H */

drivers/mmc/Kconfig
@@ -415,6 +415,12 @@ config MMC_UNIPHIER
 	  This selects support for the Matsushita SD/MMC Host Controller on
 	  SocioNext UniPhier SoCs.
 
+config SH_SDHI
+	bool "SuperH/Renesas ARM SoCs on-chip SDHI host controller support"
+	depends on ARCH_RENESAS
+	help
+	  Support for the on-chip SDHI host controller on SuperH/Renesas ARM SoCs platform
+
 config RENESAS_SDHI
 	bool "Renesas R-Car SD/MMC Host Controller support"
 	depends on ARCH_RENESAS

drivers/mmc/Makefile
@@ -49,6 +49,7 @@ obj-$(CONFIG_MMC_PCI)			+= pci_mmc.o
 obj-$(CONFIG_SUPPORT_EMMC_RPMB) += rpmb.o
 obj-$(CONFIG_MMC_SANDBOX)		+= sandbox_mmc.o
 obj-$(CONFIG_SH_MMCIF) += sh_mmcif.o
+obj-$(CONFIG_SH_SDHI) += sh_sdhi.o
 obj-$(CONFIG_STM32_SDMMC2) += stm32_sdmmc2.o
 obj-$(CONFIG_JZ47XX_MMC) += jz_mmc.o
 obj-$(CONFIG_NEXELL_DWMMC) += nexell_dw_mmc.o

drivers/pinctrl/renesas/pfc.c
@@ -951,7 +951,10 @@ static int sh_pfc_map_pins(struct sh_pfc *pfc, struct sh_pfc_pinctrl *pmx)
 static int sh_pfc_pinctrl_probe(struct udevice *dev)
 {
 	struct sh_pfc_pinctrl_priv *priv = dev_get_priv(dev);
+/* pinmux settings not defined for T2H */
+#if !defined(CONFIG_PINCTRL_PFC_RZT2H)
 	enum sh_pfc_model model = dev_get_driver_data(dev);
+#endif
 	fdt_addr_t base;
 
 	base = dev_read_addr(dev);

drivers/serial/serial_sh_rsci.h
@@ -94,7 +94,7 @@ struct uart_port {
 #elif defined(CONFIG_RCAR_GEN2) || defined(CONFIG_RCAR_GEN3) || \
       defined(CONFIG_R7S72100) || defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || \
       defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U) || defined(CONFIG_R9A09G077) || \
-	  defined(CONFIG_R9A07G076) || defined(CONFIG_R9A09G087) || defined(CONFIG_RZF_DEV)
+	  defined(CONFIG_R9A09G087) || defined(CONFIG_RZF_DEV)
 # if defined(CONFIG_SCIF_A)
 #  define SCIF_ORER	0x0200
 # else
@@ -317,7 +317,7 @@ static inline void sci_##name##_out(struct uart_port *port,\
 					sh4_scif_offset, sh4_scif_size)
 		#define SCIF_FNS(name, sh4_scif_offset, sh4_scif_size) \
 			CPU_SCIF_FNS(name, sh4_scif_offset, sh4_scif_size)
-#elif defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U) || defined(CONFIG_R9A09G077) || defined(CONFIG_R9A07G076) || \
+#elif defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U) || defined(CONFIG_R9A09G077) || \
 		defined(CONFIG_R9A09G087) || defined(CONFIG_RZF_DEV)
 	#define SCIF_FNS(reg_name, reg_offset, reg_size) \
 		CPU_SCIF_FNS(reg_name, reg_offset, reg_size)
@@ -397,7 +397,7 @@ SCIF_FNS(SCLSR,  0,  0, 0x14, 16)
 SCIF_FNS(SCLSR,  0,  0, 0x24, 16)
 #endif
 #elif defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U) || \
-		defined(CONFIG_R9A09G077) || defined(CONFIG_R9A07G076) || defined(CONFIG_R9A09G087) || defined(CONFIG_RZF_DEV)
+		defined(CONFIG_R9A09G077) || defined(CONFIG_R9A09G087) || defined(CONFIG_RZF_DEV)
 SCIF_FNS(SCSMR,  0x00, 16)
 SCIF_FNS(SCBRR,  0x02,  8)
 SCIF_FNS(SCSCR,  0x04, 16)
@@ -411,7 +411,7 @@ SCIF_FNS(SCLSR,  0x12, 16)
 SCIF_FNS(SCSEMR, 0x14,  8)
 SCIF_FNS(SCxTCR, 0x16, 16)
 
-#if defined(CONFIG_R9A09G077) || defined(CONFIG_R9A07G076) || defined(CONFIG_R9A09G087)
+#if defined(CONFIG_R9A09G077) || defined(CONFIG_R9A09G087)
 /* RSCI registers */
 SCIF_FNS(RSCxRDR, 0x00, 32)
 SCIF_FNS(RSCxTDR, 0x04, 32)

commit 491d3d89ab6854fe6e47ea796141d36ddbc79506

mmc: sh_sdhi: fix SD card detection after warm reset
On RZ/T2H and RZ/N2H the SDn_PWEN signal is left inactive after a warm
reset, so the card is never re-powered and is not detected:

  ** fs_devread read error - block
  Failed to load 'boot/Image'
  ** No partition table - mmc 1 **

The driver already power-cycles the card in probe, but the timing is too
tight:

  - The off-time was restored to 5ms by commit 46b7dae ("RZT2H and RZN2H
    sdhi updates to use mach-renesas"), silently reverting commit 5d71468
    ("mmc: sh_sdhi: fix fail to boot sd card for RZ/N2H") which had raised
    it to 6ms precisely because 5ms was not enough on RZ/N2H.

  - There is no settle time at all between re-asserting SDn_PWEN and
    sh_sdhi_initialize_common() clocking out the first command, so the
    card can still be powering up when it is first addressed.

Give the rail enough time to discharge below 0.5V as the SD Physical
Layer spec requires, and wait for the card to finish powering up before
talking to it.

Fixes: 46b7dae09153 ("RZT2H and RZN2H sdhi updates to use mach-renesas")

drivers/mmc/sh_sdhi.c
@@ -35,6 +35,20 @@
 
 #define DRIVER_NAME "sh-sdhi"
 
+/*
+ * Card power-cycle timings for RZ/T2H (R9A09G077) and RZ/N2H (R9A09G087).
+ *
+ * SH_SDHI_PWR_OFF_MS must be long enough for the card VDD rail, including the
+ * board bulk capacitance, to discharge below 0.5V as required by the SD
+ * Physical Layer spec, otherwise the card does not perform a full internal
+ * reset and keeps whatever state it was left in by the previous boot.
+ *
+ * SH_SDHI_PWR_ON_MS covers the VDD ramp plus the card's own power-up time,
+ * which must elapse before the first command is clocked out.
+ */
+#define SH_SDHI_PWR_OFF_MS	20
+#define SH_SDHI_PWR_ON_MS	10
+
 struct sh_sdhi_host {
 	void __iomem *addr;
 	int ch;
@@ -921,9 +935,17 @@ static int sh_sdhi_dm_probe(struct udevice *dev)
 	}
 
 #if ((defined CONFIG_R9A09G077) || (defined CONFIG_R9A09G087))
-	sh_sdhi_writel(host, SDHI_SD_STATUS, ~SD_STATUS_SD_PWEN & sh_sdhi_readl(host, SDHI_SD_STATUS));
-	mdelay(5);
+	/*
+	 * After a warm reset SDn_PWEN is left inactive, so the card is never
+	 * re-powered and goes undetected. Power-cycle it here, allowing the
+	 * rail time to discharge and the card time to come back up before
+	 * sh_sdhi_initialize_common() starts clocking out commands.
+	 */
+	sh_sdhi_writel(host, SDHI_SD_STATUS,
+		       ~SD_STATUS_SD_PWEN & sh_sdhi_readl(host, SDHI_SD_STATUS));
+	mdelay(SH_SDHI_PWR_OFF_MS);
 	sh_sdhi_writel(host, SDHI_SD_STATUS, SD_STATUS_SD_PWEN);
+	mdelay(SH_SDHI_PWR_ON_MS);
 #endif
 	sh_sdhi_initialize_common(host);
 