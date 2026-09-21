68f6dd0775194b35016bd287f1c735b7a7b0c71c
Asal Mirzaieva <asa.mirzaieva@canonical.com>
2026-09-21 11:01:19 +0200

    configs/smarc-rzg3e: stop blacklisting adv7511 in bootargs

--- include/configs/smarc-rzg3e.h
+++ include/configs/smarc-rzg3e.h
@@ -146,7 +146,7 @@
       "load ${devtype} ${mmcdev}:${kernel_bootpart} ${fitloadaddr} ${core_state};" \
       "env import ${fitloadaddr} ${filesize} ${recovery_vars};" \
       "if test \"${snapd_recovery_mode}\" = \"run\"; then " \
-        "setenv bootargs \"snapd_recovery_mode=${snapd_recovery_mode} ${snapd_standard_params} rw rootwait earlycon modprobe.blacklist=adv7511\";" \
+        "setenv bootargs \"snapd_recovery_mode=${snapd_recovery_mode} ${snapd_standard_params} rw rootwait earlycon\";" \
         "setenv kernel_bootpart ${mmc_boot_part};" \
         "load ${devtype} ${mmcdev}:${kernel_bootpart} ${fitloadaddr} ${core_state};" \
         "env import ${fitloadaddr} ${filesize} ${kernel_vars};" \
@@ -168,7 +168,7 @@
         "fi;" \
         "setenv kernel_prefix \"/uboot/ubuntu/${kernel_name}/\";" \
       "else " \
-        "setenv bootargs \"snapd_recovery_mode=${snapd_recovery_mode} snapd_recovery_system=${snapd_recovery_system} ${snapd_standard_params} rw rootwait earlycon modprobe.blacklist=adv7511\";" \
+        "setenv bootargs \"snapd_recovery_mode=${snapd_recovery_mode} snapd_recovery_system=${snapd_recovery_system} ${snapd_standard_params} rw rootwait earlycon\";" \
         "setenv kernel_prefix \"/systems/${snapd_recovery_system}/kernel/\";" \
       "fi;" \
       "setenv platform_part 1;"                                           \
@@ -186,7 +186,7 @@
 #endif /* UBUNTU_ENV_DEFAULT */
 #define UBUNTU_ENV_DEFAULT \
     "kernel_filename=kernel.img\0" \
-    "bootargs=modprobe.blacklist=adv7511\0" \
+    "bootargs=\0" \
     "core_state=/uboot/ubuntu/boot.sel\0" \
     "kernel_vars=snap_kernel snap_try_kernel kernel_status\0" \
     "recovery_vars=snapd_recovery_mode snapd_recovery_system snapd_recovery_kernel\0" \
